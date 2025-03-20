ff3d658060e96b54fbe6f01bda12b584708d746b
JohnGamerSmith <alexh693@gmail.com>
2025-03-19 21:39:57 -0400

    Added "Guess Note" button and its functionality. Reveal Answer button in progress.

--- app/src/main/java/com/example/sheetmusicstudy/MainActivity.kt
+++ app/src/main/java/com/example/sheetmusicstudy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sheetmusicstudy
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,7 +9,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -32,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sheetmusicstudy.ui.theme.SheetMusicStudyTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,49 +65,142 @@ fun SheetMusicPage(modifier: Modifier = Modifier) {
     val notes = arrayOf("D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5")
     val heights = arrayOf(141, 149,  158,  166,  175,  183,  192,  200,  209,  217,  226)
 
+    // 184 is B4, so maybe increments of 8?
+    // 192 is C with 1.8f scaling
+    // Not quite 9px, sometimes 8px
+
     // Default note is D4
     val defaultNoteHeight by remember { mutableStateOf(141) }
     var noteHeight by remember { mutableStateOf(defaultNoteHeight) }
+
+    // Ideally, these would be in a class somewhere, but for now they're here.
     var noteName by remember { mutableStateOf("D4") }
+    var userGuess by remember { mutableStateOf("") }
 
-    // 184 is B4, so maybe increments of 8?
-    // 192 is C with 1.8f scaling
-    // Maybe 9px difference?
+    // States: "None", "NoteSelection", "OctaveSelection".
+    var noteInputPopupState by remember { mutableStateOf("None") }
 
+    var revealBtnText by remember { mutableStateOf(R.string.reveal_button_text) }
+    // These should really be in a class
+    val inputButtonFn: () -> Unit = {
+        noteInputPopupState = "NoteSelection"
+    }
+    val noteButtonFn: (String) -> Unit = { note ->
+        userGuess = note
+        noteInputPopupState = "OctaveSelection"
+    }
+    val octaveButtonFn: (String) -> Unit = { octave ->
+        userGuess += octave
+        noteInputPopupState = "None"
+    }
+
+
+    Box(modifier = modifier) {
+        Column (
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box (modifier = Modifier.height(500.dp)) {
+                Image(painter = painterResource(R.drawable.music_staff),
+                    contentDescription = "the music staff",
+                    modifier = Modifier
+                        .align(Alignment.Center))
+
+                Image(painter = painterResource(R.drawable.quarter_note_upwards),
+                    contentDescription = "quarter note with upward stem",
+                    modifier = Modifier
+                        .scale(1.8f)
+                        .padding(start = 70.dp, bottom = noteHeight.dp)
+                        .align(Alignment.BottomStart)
+                )
+            }
+
+            Text(
+                text = userGuess,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .height(50.dp)
+
+            )
+
+            when (noteInputPopupState) {
+                "None" -> InputButton(fn = inputButtonFn)
+                "NoteSelection" -> NoteSelectionPopup(fn = noteButtonFn)
+                "OctaveSelection" -> OctaveSelectionPopup(fn = octaveButtonFn)
+            }
+
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Button(onClick = {
+                if (userGuess.length == 2) {
+                    val n = (0..10).random()
+                    noteHeight = heights[n]
+                    noteName = notes[n]
+                    revealBtnText = R.string.next_note_button_text
+                    userGuess = ""
+                }
+            }) {
+                Text(text = stringResource(revealBtnText), fontSize = 24.sp)
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun InputButton(modifier: Modifier = Modifier, fn: () -> Unit) {
+    Button(onClick = fn) {
+        Text(text = stringResource(R.string.guess_note_button_text), fontSize = 24.sp)
+    }
+    Spacer(modifier = Modifier.height(48.dp))
+}
+
+@Composable
+fun NoteSelectionPopup(modifier: Modifier = Modifier, fn: (String) -> Unit) {
+    // A row containing buttons with texts C-B
     Column (
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box (modifier = Modifier.height(500.dp)) {
-            Image(painter = painterResource(R.drawable.music_staff),
-                contentDescription = "the music staff",
-                modifier = Modifier
-                    .align(Alignment.Center))
-
-            Image(painter = painterResource(R.drawable.quarter_note_upwards),
-                contentDescription = "quarter note with upward stem",
-                modifier = Modifier
-                    .scale(1.8f)
-                    .padding(start = 70.dp, bottom = noteHeight.dp)
-                    .align(Alignment.BottomStart)
-            )
+        Row(modifier = modifier) {
+            NoteButton(note = "C", fn = fn)
+            NoteButton(note = "D", fn = fn)
+            NoteButton(note = "E", fn = fn)
+            NoteButton(note = "F", fn = fn)
         }
-        Text(
-            text = noteName,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .height(50.dp)
-
-        )
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Button(onClick = {
-            val n = (0..10).random()
-            noteHeight = heights[n]
-            noteName = notes[n]
-        }) {
-            Text(text = stringResource(R.string.next_note_button_text), fontSize = 24.sp)
+        Row {
+            NoteButton(note = "G", fn = fn)
+            NoteButton(note = "A", fn = fn)
+            NoteButton(note = "B", fn = fn)
         }
     }
 }
 
+
+@Composable
+fun NoteButton(modifier: Modifier = Modifier, note: String, fn: (String) -> Unit) {
+    Button(onClick = { fn(note) },
+        modifier = Modifier.padding(0.dp)
+    ) {
+        Text(text = note, fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun OctaveSelectionPopup(modifier: Modifier = Modifier, fn: (String) -> Unit) {
+    Row(modifier = modifier) {
+        OctaveButton(octave = "4", fn = fn)
+        OctaveButton(octave = "5", fn = fn)
+    }
+}
+
+@Composable
+fun OctaveButton(modifier: Modifier = Modifier, octave: String, fn: (String) -> Unit) {
+    Button(onClick = { fn(octave) },
+        modifier = Modifier.padding(24.dp)
+    ) {
+        Text(text = octave, fontSize = 16.sp)
+    }
+}
